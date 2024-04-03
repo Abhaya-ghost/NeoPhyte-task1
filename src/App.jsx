357c579252d22089b6e2ff5d3834b97539bff60d
abhay <abhayajeet02@gmail.com>
2024-04-03 18:03:14 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import MainComponent from './components/MainComponent'
 import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
